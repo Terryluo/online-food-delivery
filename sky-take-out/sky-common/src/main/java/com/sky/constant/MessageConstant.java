9909c726ae303bd790b23450d518ef11e1bf95a6
Zhiteng Luo(Terry) <terryluo256@gmail.com>
2024-05-26 20:48:21 -0400

    update category management

--- sky-take-out/sky-common/src/main/java/com/sky/constant/MessageConstant.java
+++ sky-take-out/sky-common/src/main/java/com/sky/constant/MessageConstant.java
@@ -11,8 +11,8 @@ public class MessageConstant {
     public static final String ALREADY_EXISTS = "Already Exists";
     public static final String UNKNOWN_ERROR = "Unknown Error";
     public static final String USER_NOT_LOGIN = "User Not Login";
-    public static final String CATEGORY_BE_RELATED_BY_SETMEAL = "Category is Related by Set Meal, Cannot Delete";
-    public static final String CATEGORY_BE_RELATED_BY_DISH = "当前分类关联了菜品,不能删除";
+    public static final String CATEGORY_BE_RELATED_BY_SETMEAL = "A setmeal/setmeals related to current category. Cannot delete this category";
+    public static final String CATEGORY_BE_RELATED_BY_DISH = "A dish/dishes related to current category. Cannot delete this category";
     public static final String SHOPPING_CART_IS_NULL = "购物车数据为空，不能下单";
     public static final String ADDRESS_BOOK_IS_NULL = "用户地址为空，不能下单";
     public static final String LOGIN_FAILED = "登录失败";
