e804014d34ecf71c76edb524e514819b269f4e49
Zhiteng Luo(Terry) <terryluo256@gmail.com>
2024-06-13 20:59:20 -0400

    constants

--- online-delivery-backend/online-delivery-common/src/main/java/com/sky/constant/AutoFillConstant.java
+++ online-delivery-backend/online-delivery-common/src/main/java/com/sky/constant/AutoFillConstant.java
@@ -1,11 +1,11 @@
 package com.sky.constant;
 
 /**
- * 公共字段自动填充相关常量
+ * common-constant
  */
 public class AutoFillConstant {
     /**
-     * 实体类中的方法名称
+     * common-constant
      */
     public static final String SET_CREATE_TIME = "setCreateTime";
     public static final String SET_UPDATE_TIME = "setUpdateTime";
--- online-delivery-backend/online-delivery-common/src/main/java/com/sky/constant/PasswordConstant.java
+++ online-delivery-backend/online-delivery-common/src/main/java/com/sky/constant/PasswordConstant.java
@@ -1,7 +1,7 @@
 package com.sky.constant;
 
 /**
- * 密码常量
+ * default password
  */
 public class PasswordConstant {
 
